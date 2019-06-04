51e059274cced80b9fd6248e8521aa4ab75dedb5
yumasnor <yumasnor@gmail.com>
2019-06-04 10:44:53 +0545

    splash screen again

--- app/src/main/java/com/example/gluttony/SplashScreen.java
+++ app/src/main/java/com/example/gluttony/SplashScreen.java
@@ -1,13 +1,19 @@
 package com.example.gluttony;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Intent intent = new Intent(SplashScreen.this,MainActivity.class );
+        startActivity(intent);
     }
 }
